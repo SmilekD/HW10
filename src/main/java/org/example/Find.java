9bb253aa582087dd53e806bce7c879b27ad9efc0
David Smilek <smilek.david13@seznam.cz>
2024-05-12 11:08:35 +0200

    Testování

--- src/main/java/org/example/Find.java
+++ src/main/java/org/example/Find.java
@@ -1,2 +1,14 @@
-package org.example;public class Find {
+package org.example;
+
+import java.util.List;
+
+public class Find {
+    public static Double findMatching(List<Double> list, Double lowerlimit, Double upperlimit){
+        for (Double item : list){
+            if (item >= lowerlimit && item <= upperlimit){
+                return item;
+            }
+        }
+        return null;
+    }
 }
